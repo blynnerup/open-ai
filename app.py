947e6839b4b4e524bd2921d7eb3c444bb41f71cf
Brian Lynnerup Pedersen <lynnerup@live.com>
2024-01-27 23:41:40 +0100

    Adds logic to create corrected transsctiptions.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import tkinter
 import customtkinter
 from tkinter.filedialog import askopenfilename
 from tkinter import filedialog
 import whisper
+import openai
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
 
 # Functions
 def openFileBrowser():
@@ -14,36 +17,72 @@ def openFileBrowser():
     except:
         print('geting file failed')
     shouldEnableTranscribeButton()
+    setDefaultFileName()
 
 def selectDestinationFolder():
     try:
-        folder = filedialog.askdirectory()
+        folder = filedialog.askdirectory() + "/"
         transcription_file_path.set(folder)
         print(transcription_file_path.get())        
     except:
         print('setting destination folder failed')
     shouldEnableTranscribeButton()
 
+def setDefaultFileName():
+    default_name = audio_file_path.get().rsplit('/', 1)[-1]
+    default_name_update_filetype = default_name.rsplit('.', 1)[0] + "_text.txt"
+    print(default_name_update_filetype)
+    output_file_name.set( default_name_update_filetype)
+    print(default_name)
+
 def shouldEnableTranscribeButton():
     path = audio_file_path.get()
     folder = transcription_file_path.get()
     if path and folder:
-        print("true")
-        # transcribe_button['state'] = "normal"
         transcribe_button.configure(state="normal")
         transcribe_button.configure(text_color="white")
     else:
-        print("false")
         transcribe_button['state'] = "disbaled"
         transcribe_button.configure(text_color="gray")
 
 def transcribeFile():
-    print('click')
+    updateProgramOutput("Transcribing")
     # Do Whisper
     model = whisper.load_model("base")
-    result = model.transcribe(audio_file_path.get())
+    result = model.transcribe(audio_file_path.get(), language="da")
+    
+    with open(transcription_file_path.get() + output_file_name.get(), "w", encoding="utf-8") as file:
+        file.write(result["text"])
+        file.close()
+    # print(result["text"])
 
-    print(result["text"])
+    updateProgramOutput("Done")
+    generate_corrected_transcript()
+
+def updateProgramOutput(updateMessage):
+    textbox.configure(state="normal")
+    textbox.insert('end', updateMessage + "\n")
+    textbox.configure(state="disabled")
+    app.update()
+
+def generate_corrected_transcript():
+    # print(transcription_file_path.get() + output_file_name.get())
+    updateProgramOutput("Preparing to correct file contents")
+    with open("Hobbitten_dk_text2.txt", encoding="utf-8") as f: text_file_content = f.read()
+    #system_prompt = "You are correcting a transcript of an audio file, where a person have read the first passages of the book The Hobbit in danish. Your task is to correct the transcribed text so that false words are in line with the text in The Hobbit, still in danish."
+    system_prompt = "Can you tell me from which book the text is from? The text is in danish."
+    corrected_transcript = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": text_file_content}
+        ],
+        max_tokens=100,
+    )
+    updateProgramOutput("File has been corrected.")
+    content = corrected_transcript.choices[0].message.content
+    print(corrected_transcript.choices[0].message.content)
+    
 
 # Systems settings
 customtkinter.set_appearance_mode("System")
@@ -51,46 +90,57 @@ customtkinter.set_default_color_theme("blue")
 
 # App frame
 app = customtkinter.CTk()
-app.geometry("720x480")
+app.geometry("760x480")
 app.title("Audio to Text")
+app.grid
 
 audio_file_path = customtkinter.StringVar(value='')
 transcription_file_path = customtkinter.StringVar(value='')
+output_file_name = customtkinter.StringVar(value='')
 
 # Label for path to audio source file
 label = customtkinter.CTkLabel(app, text="Select audio file for transcription", font=('Trebuchet MS', 18))
-label.pack(padx=10, pady=10, side="top", anchor="nw")
+label.grid(row=0, column=0, padx=10, sticky="nw")
 
 # Audio file source path
-audio_file_entry = customtkinter.CTkEntry(app, width=350, height=40, textvariable=audio_file_path)
-audio_file_entry.pack(padx=10, side="top", anchor="nw")
+audio_file_entry = customtkinter.CTkEntry(app, width=350, height=40, textvariable=audio_file_path, state="disabled")
+audio_file_entry.grid(row=1, column=0, padx=10, sticky="nw")
 
 # File browser button
 select_source_btn = customtkinter.CTkButton(app, text="Select File", command=openFileBrowser)
-select_source_btn.pack(padx = 10, pady=10, side="top", anchor="nw")
+select_source_btn.grid(row=2, column=0, padx=10, pady=10, sticky="nw")
 
 # Label for destination folder for transcribed file
-destination_folder = customtkinter.CTkLabel(app, text="Selected folder for transcribed file")
-label.pack(padx = 10, pady=10, side="top", anchor="nw")
+destination_folder = customtkinter.CTkLabel(app, text="Selected folder for transcribed file", font=('Trebuchet MS', 18))
+destination_folder.grid(row=3, column=0, padx=10, sticky="nw")
 
 # Destination folder for transcribed file
-selected_folder = customtkinter.CTkEntry(app, width=350, height=40, textvariable=transcription_file_path)
-selected_folder.pack(padx=10, pady=10, side="top", anchor="nw")
+selected_folder = customtkinter.CTkEntry(app, width=350, height=40, textvariable=transcription_file_path, state="disabled")
+selected_folder.grid(row=4, column=0, padx=10, sticky="nw")
 
 # Folder browser button
 select_folder_btn =  customtkinter.CTkButton(app, text="Select folder", command=selectDestinationFolder)
-select_folder_btn.pack(padx=10, pady=10, side="top", anchor="nw")
+select_folder_btn.grid(row=5, column=0, padx=10, pady=10, sticky="nw")
+
+# Label for optional file name
+output_file_name_label = customtkinter.CTkLabel(app, text="(Optional) Provide file name", font=('Trebuchet MS', 18))
+output_file_name_label.grid(row=6, column=0, padx=10, sticky="nw")
+
+# Optional output file name
+output_file_name_entry = customtkinter.CTkEntry(app, width=350, height=40, textvariable=output_file_name)
+output_file_name_entry.grid(row=7, column=0, padx=10, sticky="nw")
 
 # Transcribe button
 transcribe_button = customtkinter.CTkButton(app, text="Transcribe", command=transcribeFile, state="disabled")
-transcribe_button.pack(padx=10, pady=10, side="top", anchor="nw")
+transcribe_button.grid(row=8, column=0, padx=10, pady=10, sticky="nw")
+
+# Label for progress
+progress_label = customtkinter.CTkLabel(app, text="Program output", font=('Trebuchet MS', 18))
+progress_label.grid(row=0, column=1, padx=10, sticky="nw")
 
 # Textbox
-textbox = customtkinter.CTkTextbox(app, width=350, height=35)
-textbox.insert("0.0", "new text to insert")  # insert at line 0 character 0
-text = textbox.get("0.0", "end")  # get text from line 0 character 0 till the end
-textbox.configure(state="disabled")  # configure textbox to be read-only
-textbox.pack(padx=10, pady=10, side="top", anchor="nw")
+textbox = customtkinter.CTkTextbox(app, width=350, height=300)
+textbox.grid(row=1, column=1, rowspan=7, padx=10, sticky="ne")
 
 # Run app
 app.mainloop()
--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,41 @@
-import whisper
+# import whisper
 
-model = whisper.load_model("base")
+# model = whisper.load_model("base")
 
-res = model.transcribe("Dansk.m4a")
-print(res)
+# res = model.transcribe("Dansk.m4a")
+# print(res)
+
+import customtkinter
+
+def updateText():
+    textbox.configure(state="normal")
+    textbox.insert('end', "sometext")
+    textbox.configure(state="disabled")
+
+def deleteText():
+    textbox.configure(state="normal")
+    textbox.delete(0.0, 'end')
+    textbox.configure(state="disabled")
+
+# Systems settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# App frame
+app = customtkinter.CTk()
+app.geometry("760x480")
+app.title("Audio to Text")
+
+textbox = customtkinter.CTkTextbox(app, width=350, height=300, state="disabled")
+# textbox.insert("0.0", "new text to insert")  # insert at line 0 character 0
+# text = textbox.get("0.0", "end")  # get text from line 0 character 0 till the end
+# textbox.configure(state="disabled")  # configure textbox to be read-only
+textbox.pack(padx=10, pady=10, side="top", anchor="nw")
+
+transcribe_button = customtkinter.CTkButton(app, text="Transcribe", command=updateText)
+transcribe_button.pack(padx=10, pady=10, side="top", anchor="nw")
+
+transcribe_button_delete = customtkinter.CTkButton(app, text="Transcribe", command=deleteText)
+transcribe_button_delete.pack(padx=10, pady=10, side="top", anchor="nw")
+# Run app
+app.mainloop()
